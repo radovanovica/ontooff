9742e1d72c592a39f6aa6be907e95ae94e7827bd
Aleksandar Radovanovic <radovanovica1993@gmail.com>
2026-04-15 17:00:04 +0200

    fix for oauth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -22,6 +22,7 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      allowDangerousEmailAccountLinking: true,
       profile(profile) {
         return {
           id: profile.sub,
